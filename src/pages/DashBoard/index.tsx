961c29d26ef13e81d88421a67d0f4766dc949e5f
Samuel-Jesus <samuel.jesan2018@gmail.com>
2021-03-30 23:09:43 -0300

    Merge para revertos outros commits

---
+++ src/pages/DashBoard/index.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Switch, Route} from "react-router-dom";
+import MainLayout from "../../layout/MainLayout";
+
+import Home from "./Home";
+import Patient from "./Patient";
+import Consultations from "./Consultations";
+import PatientRecord from "./PatientRecord";
+
+const Dashboard: React.FC = () => {
+	return (
+		<MainLayout>
+			<Switch>
+				<Route path="/dashboard" exact={true} component={Home} />
+				<Route path="/dashboard/patient" component={Patient} />
+				<Route path="/dashboard/consultations" component={Consultations} />
+				<Route path="/dashboard/patientRecord" component={PatientRecord} />
+			</Switch>
+		</MainLayout>
+	);
+}
+
+export default Dashboard;
---
+++ src/services/Api/ApiTypes/index.ts
@@ -0,0 +1,194 @@
+import { Api, config } from "../Connection";
+import { AxiosResponse } from "axios";
+const API = Api();
+
+export interface Anamnesis {
+    id: string;
+    qp: string;
+    hda: string;
+    hpp: string;
+    hps: string;
+    hf: string;
+    patientRecordsId: string;
+    createdBy: string;
+    modifiedBy: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+export interface PhysicalExam {
+    id: string;
+    general: string;
+    skin: string;
+    headAndNeck: string;
+    sysRespiratory: string;
+    sysCardiovascular: string;
+    sysAbdominal: string;
+    sysUrinary: string;
+    sysNeurological: string;
+    extremities: string;
+    osteoarticular: string;
+    mentalState: string;
+    fnCortical: string;
+    cranialNerves: string;
+    motricity: string;
+    sensory: string;
+    reflections: string;
+    cerebellar: string;
+    march: string;
+    others: string;
+    patientRecordsId: string;
+    createdBy: string;
+    modifiedBy: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+export enum ConsultType {
+    return = "Retorno",
+    first = "Primeira",
+}
+
+interface IConsult {
+    id: string;
+    professional: string;
+    consultType: ConsultType;
+    time: Date;
+    observation: string;
+    patientId: string;
+    createdBy: string;
+    modifiedBy: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+export interface ICreateConsults {
+    professional: string;
+    consultType: ConsultType;
+    time: string;
+    observation: string;
+    patientId: string;
+    createdBy: string;
+    modifiedBy: string;
+}
+
+export interface IPatient {
+    id: string;
+    name: string;
+    email: string;
+    gender: string;
+    age: number;
+    color: string;
+    birthdate: Date;
+    scholarity: string;
+    profession: string;
+    nationality: string;
+    income: string;
+    primaryPhone: string;
+    secondPhone: string;
+    knowUs: string;
+    healthPlan: string;
+    companions: string;
+    observation: string;
+    patientRecordId: string[];
+    createdBy: string;
+    modifiedBy: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+export interface ICreatePatient {
+    name: string;
+    email: string;
+    gender: string;
+    age: number;
+    color: string;
+    birthdate: Date;
+    scholarity: string;
+    profession: string;
+    nationality: string;
+    income: string;
+    primaryPhone: string;
+    secondPhone: string;
+    knowUs: string;
+    healthPlan: string;
+    companions: string;
+    observation: string;
+    patientRecordId: string[];
+    createdBy: string;
+    modifiedBy: string;
+}
+
+export enum Permission {
+    admin = "admin",
+    user = "user",
+}
+
+export interface IUser {
+    id: string;
+    name: string;
+    cpf: string;
+    certificate: string;
+    speciality: string;
+    permission: Permission; // AQUI ALGUM VALOR DO ENUM PERMISSION QUE ESTÁ ACIMA DA INTERFACE
+    profession: string;
+    email: string;
+    token: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+export interface ILoginUser {
+    email: string;
+    password: string;
+}
+
+export interface IPatientRecord {
+    id: string;
+    patientId: IPatient;
+    createdBy: string;
+    modifiedBy: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+export interface ICreatePatientRecord {
+    patientId: string;
+    createdBy: string;
+    modifiedBy: string;
+    general: string;
+    skin: string;
+    headAndNeck: string;
+    sysRespiratory: string;
+    sysCardiovascular: string;
+    sysAbdominal: string;
+    sysUrinary: string;
+    sysNeurological: string;
+    extremities: string;
+    osteoarticular: string;
+    mentalState: string;
+    fnCortical: string;
+    cranialNerves: string;
+    motricity: string;
+    sensory: string;
+    reflections: string;
+    cerebellar: string;
+    march: string;
+    others: string;
+    qp: string;
+    hda: string;
+    hpp: string;
+    hps: string;
+    hf: string;
+}
+
+export const getPatients = async (): Promise<AxiosResponse<IPatient[]>> => await API.get("patients", config);
+export const createPatient = async (sendValue: ICreatePatient): Promise<IPatient> => await API.post("patients/create", sendValue, config);
+
+export const getConsults = async (): Promise<AxiosResponse<IConsult[]>>  => await API.get("consults", config);
+export const createConsult = async (sendValue: ICreateConsults) => await API.post("consults/create", sendValue, config);
+
+export const login = async (sendValue: ILoginUser): Promise<AxiosResponse<IUser>> => API.post("/login", sendValue,config);
+
+export const getPatientRecords = async (): Promise<AxiosResponse<IPatientRecord[]>> => await API.get("patientRecords", config);
+export const createPatientRecord = async (sendValue: ICreatePatientRecord): Promise<IPatientRecord> => await API.post("patientRecords/create", sendValue, config);
