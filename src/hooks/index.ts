aae9f42a0b40b84ef54e20551b181c30ecce93b4
Samuel-Jesus <samuel.jesan2018@gmail.com>
2021-03-30 22:56:23 -0300

    Revert "comitando pra deletar isso"
    This reverts commit 231de9a4a478192636f69dbc7056571ca9b2d4e8, reversing
    changes made to 9d8a348c81c1e66dfb4588d379859bf5953e4e70.

--- src/hooks/index.ts
+++
@@ -1,6 +0,0 @@
-import useForm from "./useForm";
-import useSnackBar from "./useSnackBar";
-import useTableForm from "./useTableForm";
-import useLoader from "./useLoader";
-
-export { useForm, useSnackBar, useTableForm, useLoader } ;
