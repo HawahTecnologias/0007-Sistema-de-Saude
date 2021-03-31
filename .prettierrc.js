c6bda8737a9b0e3ad4d82c90870f73e6a1259e64
Samuel-Jesus <samuel.jesan2018@gmail.com>
2021-03-30 23:03:28 -0300

    Revert "comitando pra deletar isso"
    This reverts commit 231de9a4a478192636f69dbc7056571ca9b2d4e8, reversing
    changes made to 9d8a348c81c1e66dfb4588d379859bf5953e4e70.

--- .prettierrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	semi: true,
-	trailingComma: "all",
-	singleQuote: false,
-	printWidth: 120,
-	tabWidth: 4,
-	useTabs: true,
-	printWidth: 80
-};
--- src/services/Api/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./ApiTypes";
-
-export * from "./Connection";
