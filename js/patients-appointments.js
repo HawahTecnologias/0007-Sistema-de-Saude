770aa341ef57c65d3f0cfa1092ada7424119c2a1
Samuel-Jesus <samuel.jesan2018@gmail.com>
2021-09-03 01:08:10 -0300

    adding script for attendance and lacation profile

--- js/patients-appointments.js
+++ js/patients-appointments.js
@@ -1,6 +1,6 @@
 // Scripts for list registered patients 
 $(document).ready(function(){
-    function listAppointment(id, name, birthdate, email, appointment, type, status, cor) {
+    function listAppointment(ids, name, birthdate, email, appointment, type, status, cor) {
         let list = $("#list-appointments")
         
         let div = document.createElement('div')
@@ -13,12 +13,12 @@ $(document).ready(function(){
         let a = document.createElement('a')
         let a2 = document.createElement('a')
         let a3 = document.createElement('a')
-        let a4 = document.createElement('a')
-        //let a5 = document.createElement('a')
+        // let a4 = document.createElement('a')
+        // let a5 = document.createElement('a')
     
         let i = document.createElement('i')
-        let i2 = document.createElement('i')
-        //let i3 = document.createElement('i')
+        // let i2 = document.createElement('i')
+        // let i3 = document.createElement('i')
     
         let img = document.createElement('img')
     
@@ -46,13 +46,13 @@ $(document).ready(function(){
     
         a.classList.add('d-flex', 'card-img')
         a2.classList.add('list-item-heading', 'mb-1', 'truncate', 'text-center', 'w-25', 'w-xs-100')
-        a3.classList.add('eye')
-        a4.classList.add('edit')
-        //a5.classList.add('list-delete')
+        a3.classList.add('show-profile', 'c-pointer')
+        // a4.classList.add('edit-profile', 'c-pointer')
+        // a5.classList.add('list-delete')
     
-        i.classList.add('ik', 'ik-eye')
-        i2.classList.add('ik', 'ik-edit-2')
-        //i3.classList.add('ik', 'ik-trash-2')
+        i.classList.add('ik', 'ik-user')
+        // i2.classList.add('ik', 'ik-edit-2')
+        // i3.classList.add('ik', 'ik-trash-2')
     
         img.classList.add('list-thumbnail', 'responsive', 'border-0')
     
@@ -64,13 +64,20 @@ $(document).ready(function(){
         p3.classList.add('mb-1', 'text-muted', 'text-small', 'date', 'text-center', 'w-20', 'w-xs-100')
         {/* p4.classList.add('mb-1', 'text-muted', 'text-small', 'date', 'text-center', 'w-1', 'w-xs-100') */}
 
-        button.classList.add('mr-5', 'btn', 'btn-outline-success')
+        button.classList.add('mr-5', 'btn', 'btn-outline-success', 'atender')
         button2.classList.add('btn', 'btn-outline-secondary')
-        
+
         button.setAttribute('type', 'button')
         button2.setAttribute('type', 'button')
 
-        a2.setAttribute('id',id)
+        a3.setAttribute('title', 'Ver perfil do paciente')
+
+        let id = ids.split('-')
+        a2.setAttribute("id",id[0] )
+        a2.setAttribute("data-id_patient",id[1] )
+        a2.setAttribute("data-id_user", id[2] )
+        a2.setAttribute("data-id_record", id[3] )
+        a2.setAttribute("data-id_stage", id[4] )
     
         span.innerHTML = type;
         a2.innerHTML = name;
@@ -97,12 +104,12 @@ $(document).ready(function(){
                         // p.append(span2)
                         p2.append(span3)
                         div6.append(span6)
-                    div5.append(a3, a4)
+                    div5.append(a3)
                     a3.append(i)
-                    a4.append(i2)
+                    // a4.append(i2)
                     // a5.append(i3)
-    console.log(cor)
     }
+    
     const html = {
         get(element){
             return document.querySelector(element);
@@ -111,8 +118,9 @@ $(document).ready(function(){
 
     function myAge(birthdate, newdate){ return Math.floor(Math.ceil(Math.abs(birthdate.getTime() - newdate.getTime()) / (1000 * 3600 * 24)) / 365.25);}
 
-    getAppointment('1', '10')
-     function getAppointment(page, perpage) {
+        getAppointment('1', '10')
+//Fuction for pagination of content on the page
+    function getAppointment(page, perpage) {
   
         let request = $.ajax({
                 type: "GET",
@@ -132,8 +140,8 @@ $(document).ready(function(){
                 localStorage.setItem("navlastPage", dados.lastPage);
                 localStorage.setItem("navfirstPage", dados.firstPage);
                 console.log(response)
-                dado.forEach(agendamento => {
-                    switch(agendamento.type) {
+                dado.forEach(appo => {
+                    switch(appo.type) {
                         case 'teleconsultation':
                             var type = "Tele Consulta"
                           break;
@@ -141,7 +149,7 @@ $(document).ready(function(){
                              type = "Presencial"
                           break;
                     }
-                    switch(agendamento.status) {
+                    switch(appo.status) {
                         case 'scheduled':
                             var status = "Agendado"
                             var cor = "badge-info"
@@ -172,16 +180,15 @@ $(document).ready(function(){
                           break;
                     }
                     
-                            let dia = agendamento.start_date.substring(0,10)
-                            let hora = agendamento.start_date.substring(11,16)
-                            let dia_fim = agendamento.end_date.substring(0,10)
-                            let hora_fim = agendamento.end_date.substring(11,16)
-
+                            let dia = appo.start_date.substring(0,10)
+                            let hora = appo.start_date.substring(11,16)
+                            let dia_fim = appo.end_date.substring(0,10)
+                            let hora_fim = appo.end_date.substring(11,16)
                             
     
                         $.ajax({
                             type: "GET",
-                            url: url+"patient/"+agendamento.patient_id,
+                            url: url+"patient/"+appo.patient_id,
                             headers: { 'Authorization': 'Bearer ' + auth },
                             data: {
                                 
@@ -195,7 +202,7 @@ $(document).ready(function(){
 
                                 let names = res.first_name+" "+res.last_name
                                
-                                listAppointment(res.id, names, `(${age} anos) / ${birthdate}`, res.email,`${dia} | ${hora}-${hora_fim}`, type, status, cor)      
+                                listAppointment(`${appo.id}-${appo.patient_id}-${appo.user_id}-${appo.record_id}-${appo.stage}`, names, `(${age} anos) / ${birthdate}`, res.email,`${dia} | ${hora}-${hora_fim}`, type, status, cor)      
                                      
                             },
                             error: function(xhr, ajaxOptions, thrownError) {
@@ -211,12 +218,72 @@ $(document).ready(function(){
             }
             })
     };
+// Script to forward to patient profile according to patient id and appointment id
+    $(document).on('click', '.show-profile', function(e) {
+        e.preventDefault;
+        let id_appointment = $(this).parent().siblings('.card-body').find('a').attr('id');
+        let id_patient = $(this).parent().siblings('.card-body').find('a[data-id_patient]').data('id_patient');
+        
+        document.location.href = `../pages/profiles.html?patient=${id_patient}&appointment=${id_appointment}`;
+    });
+//Script for initiating medical care with the patient
+    $(document).on('click', '.atender', function(e) {
+        e.preventDefault;
+
+            let id_appo = $(this).siblings('a').attr('id');
+            let id_patient = $(this).siblings('a[data-id_patient]').data('id_patient');
+            let id_user = $(this).siblings('a[data-id_user]').data('id_user')
+            let id_record = $(this).siblings('a[data-id_record]').data('id_record');
+            let id_stage = $(this).siblings('a[data-id_stage]').data('id_stage');
+
+            localStorage.setItem('id_patient_agd', id_patient)
+            localStorage.setItem('id_agd', id_appo)
+            localStorage.setItem('id_record', id_record)
+            localStorage.setItem('id_stage', id_stage)
+            console.log("Inicio da consulta")
+            
+            $("#short_list_term").html("")
+            $("#long_list_term").html("")
+            $("#problem_list_term").html("")
+            todoGet(id_patient, 'open', 'shortTerm', 'short_list_term')
+            todoGet(id_patient, 'open', 'longTerm', 'long_list_term')
+            todoGet(id_patient, 'open', 'problem', 'problem_list_term')
+            createHistoricTodo(id_patient) 
+        $.ajax({
+            type: "GET",
+            url: url+"patient/"+id_patient,
+            headers: { 'Authorization': 'Bearer ' + auth },
+            xhrFields: { withCredentials: true },
+            data: {},
+            cache: false,
+            success: function(response){
+                alterStatusAppointment(id_patient, id_appo,'inProgress', id_stage, id_record)
+                $("#fullwindowModalLabel").html(response.first_name+" "+response.last_name)
+                $("#pront_pacit_nome").html(response.first_name+" "+response.last_name)
+                $("#pront_pacit_genero").html(response.gender)
+                $("#pront_pacit_etnia").html(response.ethnicity)  
+                $("#pront_pacit_nasc").html(response.birthdate)
+                $("#pront_pacit_plano").html(response.health_insurance)
+                $("#pront_pacit_natural").html(response.city)
+                $("#pront_pacit_renda").html(response.income)
+                $("#pront_pacit_prof").html(response.occupation)
+                $("#pront_pacit_tel").html(response.cellphone+"<br>"+response.phone+"<br>"+response.second_phone)
+                $("#pront_pacit_email").html(response.email)
+                $("#pront_pacit_grau").html(response.schooling)
+                $("#pront_pacit_conheceu").html(response.how_met)
+                $("#pront_pacit_endereco").html(response.city+"/"+response.state+", "+response.street_address+" | "+response.complement)
+            },
+            error: function(xhr, ajaxOptions, thrownError) {      
+                console.log(xhr, ajaxOptions, thrownError);
+            }
+        });
+    });
 
 let perPage = $('#view-perpage').val()
 var pag = localStorage.getItem("navCurrentPage")
 let lastPage = localStorage.getItem("navlastPage")
 let firstPage = localStorage.getItem("navfirstPage")
-console.log(pag)
+
 
     html.get('.first').addEventListener('click', () =>{
         perPage = $('#view-perpage').val()
